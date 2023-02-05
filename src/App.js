cc071c2245c088ba02e0a7290c3deca111ba2863
Rafli Ramadhan <rafliramdhn@gmail.com>
2023-02-05 17:03:35 +0700

    fix size text

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
     <div>
       <div className="flex justify-center font-wondar">
         <div className="w-full lg:w-2/6 bg-slate-200 h-screen bg-cover bg-center text-white" style={{backgroundImage: `url(${home})` }}>
-          <div className="text-center pt-10">
+          <div className="text-center text-xl pt-10">
             <div>
               We are getting married
             </div>
-            <div className="font-amsterdam text-4xl mt-5">Pony & Hendi</div>
+            <div className="font-amsterdam text-5xl mt-5">Pony & Hendi</div>
             <div className="absolute bottom-20 w-full lg:w-2/6">
               <div className="grid grid-cols-4 px-7">
                 <div className="w-full border border-white py-3">
@@ -37,8 +37,8 @@ function App() {
       <div className="flex justify-center font-wondar">
         <div className="w-full lg:w-2/6 h-screen bg-slate-800 text-white px-10 pt-10 text-center space-y-10">
           <div>
-            <div className="text-3xl underline font-semibold">Akad Nikah</div>
-            <div className="grid grid-cols-1 text-xl mt-7">
+            <div className="text-4xl underline font-semibold">Akad Nikah</div>
+            <div className="grid grid-cols-1 text-2xl mt-7">
               <div>Minggu</div>
               <div className="text-3xl">19</div>
               <div>Februari 2023</div>
@@ -53,8 +53,8 @@ function App() {
           </div>
 
           <div>
-            <div className="text-3xl underline font-semibold">Resepsi Nikah</div>
-            <div className="grid grid-cols-1 text-xl mt-7">
+            <div className="text-4xl underline font-semibold">Resepsi Nikah</div>
+            <div className="grid grid-cols-1 text-2xl mt-7">
               <div>Minggu</div>
               <div className="text-3xl">19</div>
               <div>Februari 2023</div>
@@ -65,7 +65,7 @@ function App() {
               <div>
                 Jl. Bakti VI RT 001 RW 08 No. 30, Kelurahan Gaga, Kecamatan Larangan
               </div>
-              <a href='https://goo.gl/maps/nT3UVpquQAj258cUA' target="_blank" className="border border-white py-2 px-4 rounded hover:border-black hover:bg-white hover:text-black font-bold mt-10">
+              <a href='https://goo.gl/maps/nT3UVpquQAj258cUA' target="_blank" className="border border-white py-3 px-4 rounded hover:border-black hover:bg-white hover:text-black font-bold mt-10">
                 Open maps
               </a>
             </div>
@@ -75,12 +75,12 @@ function App() {
 
       <div className="flex justify-center font-wondar">
         <div className="w-full lg:w-2/6 h-screen bg-slate-200 text-black px-10 pt-10 text-center space-y-10">
-          <div className="text-2xl">WEDDING <span className="font-amsterdam">Gift</span> </div>
-          <div className="italic tracking-wide">
+          <div className="text-3xl">WEDDING <span className="font-amsterdam">Gift</span> </div>
+          <div className="italic tracking-wide text-xl">
             Kehadiran dan doa Anda akan menjadi hadiah pernikahan terbesar yang pernah kami minta. Tidak ada hadiah lain yang dibutuhkan atau diharapkan. Namun, jika memberi adalah tanda cinta, kita senang menerimanya dan tentunya akan semakin melengkapi kebahagiaan kita.
           </div>
-          <div className="grid grid-cols-1 gap-4">
-            <div className="flex bg-slate-800 text-white justify-between px-2 items-center py-4 rounded">
+          <div className="grid grid-cols-1 gap-4 text-xl">
+            <div className="flex bg-slate-800 text-white justify-between px-4 items-center py-4 rounded">
               <div>
                 <img src={bca} className="w-24" alt="" srcset="" />
               </div>
@@ -95,10 +95,10 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="italic tracking-wide">
+          <div className="italic tracking-wide text-xl">
             Anda juga dapat mengirim hadiah ke alamat ini :
           </div>
-          <div className="flex bg-slate-800 text-white justify-between px-6 space-x-5 items-center py-4 rounded">
+          <div className="flex bg-slate-800 text-white text-xl justify-between px-6 space-x-5 items-center py-4 rounded">
               <div className="text-left">
                 Jl. Bakti VI RT 001 RW 08 No. 30, Kelurahan Gaga, Kecamatan Larangan
               </div>
